415d925924ffc31ee1052f5fcbf19c7d68c5c1bc
robert <robert@gtsvn.uit.no>
2014-07-18 14:57:56 +0000

    Updating one more file for all of the cases
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@97638 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- rus/src/main/java/werti/util/HTMLEnhancer.java
+++ rus/src/main/java/werti/util/HTMLEnhancer.java
@@ -40,7 +40,12 @@ public class HTMLEnhancer {
 		HashMap<String, String> dict = new HashMap<String, String>(); // translations of topics and activities to North Sámi
 		dict.put("SubstantiveSingular", "Substantive singular");
 		dict.put("SubstantivePlural", "Substantive plural");
-		dict.put("GenNouns", "Genitive nouns");
+		dict.put("NounsNom", "Nominative nouns");
+		dict.put("NounsAcc", "Accusative nouns");
+		dict.put("NounsGen", "Genitive nouns");
+		dict.put("NounsLoc", "Locative nouns");
+		dict.put("NounsDat", "Dative nouns");
+		dict.put("NounsIns", "Instrumental nouns");
 		dict.put("PresFutInd", "Verb conjugation");
 		dict.put("NegVerbs", "Negation forms of verbs");
 		dict.put("InfiniteVerbs", "Infinite verbs");
