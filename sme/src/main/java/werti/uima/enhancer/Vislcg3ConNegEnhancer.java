54d8b6474ec5e2034d0d3ccdc441d65c66ca5bf3
chiara <chiara@gtsvn.uit.no>
2018-02-15 15:14:46 +0000

    replacing paths with var in Constants class
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@163699 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/java/werti/uima/enhancer/Vislcg3ConNegEnhancer.java
+++ sme/src/main/java/werti/uima/enhancer/Vislcg3ConNegEnhancer.java
@@ -21,11 +21,13 @@ import werti.util.EnhancerUtils;
 import werti.util.StringListIterable;
 import werti.server.WERTiServlet;
 
+import werti.util.Constants;
+
 /**
  * Use the TAG-B TAG-I sequences resulting from the CG3 analysis with
- * {@link werti.ae.Vislcg3Annotator} to enhance spans corresponding 
+ * {@link werti.ae.Vislcg3Annotator} to enhance spans corresponding
  * to the tags specified by the activity as tags of negation forms of verbs.
- * 
+ *
  * @author Niels Ott?
  * @author Adriane Boyd
  * @author Heli Uibo
@@ -35,26 +37,26 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 
 	private static final Logger log =
 		Logger.getLogger(Vislcg3ConNegEnhancer.class);
-	
+
 	private List<String> connegTags;
 	private static String CHUNK_BEGIN_SUFFIX = "-B";
 	private static String CHUNK_INSIDE_SUFFIX = "-I";
-	private final String preprocessLoc = "/home/heli/main/gt/script/preprocess";
-    private final String lookupLoc = "/usr/local/bin/lookup";
-    private final String lookupFlags = "-flags mbTT -utf8";
-	private final String invertedFST = " /opt/smi/sme/bin/isme-GG.restr.fst";
-	
+	private final String preprocessLoc = Constants.preprocess_Loc;
+  private final String lookupLoc = Constants.lookup_Loc;
+  private final String lookupFlags = Constants.lookup_Flags;
+	private final String invertedFST = Constants.inverted_FST;
+
 	/**
 	 * A runnable class that reads from a reader (that may
 	 * be fed by {@link Process}) and puts stuff read into a variable.
 	 * @author nott
 	 */
 	public class ExtCommandConsume2String implements Runnable {
-		
+
 		private BufferedReader reader;
 		private boolean finished;
 		private String buffer;
-		
+
 		/**
 		 * @param reader the reader to read from.
 		 */
@@ -64,7 +66,7 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 			finished = false;
 			buffer = "";
 		}
-		
+
 		/**
 		 * Reads from the reader linewise and puts the result to the buffer.
 		 * See also {@link #getBuffer()} and {@link #isDone()}.
@@ -80,14 +82,14 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 			}
 			finished = true;
 		}
-		
+
 		/**
 		 * @return true if the reader read by this class has reached its end.
 		 */
 		public boolean isDone() {
 			return finished;
 		}
-		
+
 		/**
 		 * @return the string collected by this class or null if the stream has not reached
 		 * its end yet.
@@ -96,12 +98,12 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 			if ( ! finished ) {
 				return null;
 			}
-			
+
 			return buffer;
 		}
-		
+
 	}
-	
+
 	@Override
 	public void initialize(UimaContext context)
 			throws ResourceInitializationException {
@@ -125,7 +127,7 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 
 		// iterating over chunkTags instead of classCounts.keySet() because it is important to control the order in which
 		// spans are enhanced
-		
+
 		for (String conT: connegTags) {
 			FSIterator cgTokenIter = cas.getAnnotationIndex(CGToken.type).iterator();
 			// remember previous token so we can getEnd() from it (chunk)
@@ -135,7 +137,7 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 			while (cgTokenIter.hasNext()) {
 				CGToken cgt = (CGToken) cgTokenIter.next();
 				if (enhancement_type.equals("cloze") || enhancement_type.equals("mc")) {
-                                    // more than one reading? don't mark up if exercise type is mc or cloze                       
+                                    // more than one reading? don't mark up if exercise type is mc or cloze
                                     if (!isSafe(cgt)) {
                                         continue;
                                     }
@@ -143,9 +145,9 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 
 				// analyze reading(s)
 				for (int i=0; i < cgt.getReadings().size(); i++) { // Loop over all the readings. If there is one analysis that matches the tag pattern then the token will be selected for the exercise.
-				    CGReading reading = cgt.getReadings(i); 
+				    CGReading reading = cgt.getReadings(i);
 				    //log.info("next reading: "+reading);
-		
+
 				    if (containsTag(reading, conT)) {
 					// get lemma from the reading
 					String lemma = getLemma(reading);
@@ -156,12 +158,12 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 					e.setRelevant(true);
 					e.setBegin(cgt.getBegin());
 					e.setEnd(cgt.getEnd());
-					
+
 					// increment id
 					newId = classCounts.get(conT) + 1;
 					String spanStartTag = "<span id=\"" + EnhancerUtils.get_id("WERTi-span-" + conT, newId) + "\" class=\"wertiviewtoken  wertiviewConNeg \" lemma=\"" + lemma + "\" distractors=\"" + distractors + "\" >";
 					log.info(spanStartTag);
-					e.setEnhanceStart(spanStartTag);					
+					e.setEnhanceStart(spanStartTag);
 					e.setEnhanceEnd("</span>");
 					classCounts.put(conT, newId);
 					//log.info(newId);
@@ -177,21 +179,21 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 				//prev = cgt;
 			}
 		}
-		
+
 
 		// (chunk)
 		//log.info("Enhancement stack is "
 		//		+ (enhancements.empty() ? "empty, OK" : "not empty, WTF??"));
 		log.info("Finished conjunction enhancement");
 	}
-	
+
 	/*
 	 * Determines whether the given token is safe, i.e. unambiguous
 	 */
 	private boolean isSafe(CGToken t) {
 		return t.getReadings() != null && t.getReadings().size() == 1;
 	}
-	
+
 	/*
 	 * Determines whether the given reading contains the given tag
 	 */
@@ -201,16 +203,16 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 		for (String rtag : reading) {
 			reading_str = reading_str + rtag + " ";
 		}
-		
+
 		if (reading_str.indexOf(tag) > 0) {  // Tag string contains Ind Prs ConNeg or Ind Prt ConNeg
             log.info(cgr + " contains " + tag);
             return true;
         }
-		
+
 		//log.info(cgr + " does not contain " + tag);
 		return false;
 	}
-	
+
 	private String getLemma(CGReading cgr) {
 		StringListIterable reading = new StringListIterable(cgr);
 		String lemma = "", lemma_utf8 = "";
@@ -233,22 +235,22 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 		//log.info(cgr + " does not contain " + tag);
 		return lemma;
 	}
-	
+
 	private String getDistractors(String lemma) {
 	   String[] distract_forms = {"V+Ind+Prs+Sg1", "V+Ind+Prs+Sg2", "V+Ind+Prs+Sg3", "V+Ind+Prt+Sg1", "V+Ind+Prt+Sg2", "V+Ind+Prt+Sg3"};
-	   
+
 		String str, word, result = "", generationInput = "";
-        
+
         for (int j=0; j < distract_forms.length; j++) {
             generationInput += lemma + "+" + distract_forms[j] + "\n";
             //generationInput += lemma + "+v1+" + distract_forms[j] + "\n";
         }
         String[] generationPipeline = {"/bin/sh", "-c", "/bin/echo \"" + generationInput + "\" | " + lookupLoc + " " + lookupFlags + " " + invertedFST};
-		
+
         log.info("Form generation pipeline: "+generationPipeline[2]);
         try {
 			Process process = Runtime.getRuntime().exec(generationPipeline);
-			
+
 			BufferedReader fromIFST = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF8"));
 			ExtCommandConsume2String stdoutConsumer = new ExtCommandConsume2String(fromIFST);
 			Thread stdoutConsumerThread = new Thread(stdoutConsumer, "FST STDOUT consumer");
@@ -259,7 +261,7 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 				log.error("Error in joining output consumer of VislCG with regular thread, going mad.", e);
 				return null;
 			}
-			
+
 			fromIFST.close();
 			String iFSToutput = stdoutConsumer.getBuffer();
 			StringTokenizer tok = new StringTokenizer(iFSToutput);
@@ -270,24 +272,24 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
 					result = result + word + " ";
 				}
 			}
-			
+
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        
-        log.info("Generated forms read from the outputfile: "+result);	  
+
+        log.info("Generated forms read from the outputfile: "+result);
         return result;
     }
-}	
+}
 		/* String str, word, result = "";
-	   // get timestamp in milliseconds and use it in the names of the temporary files in order to avoid conflicts between simultaneous users                                                                                            
+	   // get timestamp in milliseconds and use it in the names of the temporary files in order to avoid conflicts between simultaneous users
         long timestamp = System.currentTimeMillis();
 
-        String inputfileLoc = "/Users/mslm/main/apps/teaksta/sme/output/iFSTinput"+timestamp+".tmp";
-        String outputfileLoc = "/Users/mslm/main/apps/teaksta/sme/output/iFSToutput"+timestamp+".tmp";
+        String inputfileLoc = "/Users/car010/main/apps/teaksta/sme/output/iFSTinput"+timestamp+".tmp";
+        String outputfileLoc = "/Users/car010/main/apps/teaksta/sme/output/iFSToutput"+timestamp+".tmp";
 
-        //create temporary files for saving cg3 input and output                                                   
+        //create temporary files for saving cg3 input and output
 
         Writer inputfile = null;
 
@@ -303,15 +305,15 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
         }
         catch (IOException e) {
         System.out.println(e.getMessage());
-        }	   
-	   
+        }
+
 	   String[] generationPipeline = {"/bin/sh", "-c", "/bin/cat " + inputfileLoc + " | " + lookupLoc + " " + lookupFlags + " " + invertedFST + " > " + outputfileLoc};
-	   
+
         log.info("Form generation pipeline: "+generationPipeline[2]);
         try {
             Process process = Runtime.getRuntime().exec(generationPipeline);
             process.waitFor();
-        	
+
             BufferedReader outputfile = new BufferedReader(new InputStreamReader(new FileInputStream(outputfileLoc), "UTF8"));
 
             while ((str = outputfile.readLine()) != null) {
@@ -324,7 +326,7 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
               }
             }
             log.info("Generated forms read from the outputfile: "+result);
-        
+
             outputfile.close();
             // Delete the temporary files:
             boolean inputfiledeleted = (new File(inputfileLoc)).delete();
@@ -338,7 +340,7 @@ public class Vislcg3ConNegEnhancer extends JCasAnnotator_ImplBase {
         }
         catch (IOException e) {
         System.out.println(e.getMessage());
-        }	  
+        }
 
         return result;
     }
