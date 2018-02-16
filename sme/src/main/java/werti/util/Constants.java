b23badeeda8a42da1ee5d16a71f0a3516f561031
chiara <chiara@gtsvn.uit.no>
2018-02-16 10:11:54 +0000

    add static to var
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@163712 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/java/werti/util/Constants.java
+++ sme/src/main/java/werti/util/Constants.java
@@ -25,20 +25,20 @@ public final class Constants {
   */
 
   //GTOAHPA PATHS:
-    public final String lookup_Loc = "/usr/bin/lookup";
-    public final String lookup_Flags = "-flags mbTT -utf8";
+    public static final String lookup_Loc = "/usr/bin/lookup";
+    public static final String lookup_Flags = "-flags mbTT -utf8";
 
-    public final String inverted_FST = " /opt/smi/sme/bin/isme-GG.restr.fst"; // opt/smi/sme/bin on gtlab;
-    public final String an_FST = " /opt/smi/sme/bin/sme.fst";
+    public static final String inverted_FST = " /opt/smi/sme/bin/generator-oahpa-gt-norm-dial_GG.xfst"; // opt/smi/sme/bin on gtlab;  or " /opt/smi/sme/bin/isme-GG.restr.fst"
+    public static final String an_FST = " /opt/smi/sme/bin/sme.fst";
 
     public static final String tools_Dir = "/opt/smi/sme/bin/";
     public static final String abbr_Dir = "/opt/smi/sme/bin/";
-    public final String abbr_file = " --abbr=/opt/smi/sme/bin/abbr.txt | --corr=/opt/smi/sme/bin//corr.txt | ";
+    public static final String abbr_file = " --abbr=/opt/smi/sme/bin/abbr.txt | --corr=/opt/smi/sme/bin//corr.txt | ";
 
-    public final String preprocess_Pipeline = "/home/heli/main/gt/script/preprocess"; // not sure if this is correct, anyway it seems the var is not in use
-  	public final String preprocess_Loc = "/opt/smi/sme/bin/preprocess";
-  	public final String lookup_2cgLoc = " | /opt/smi/sme/bin/lookup2cg | ";
+    public static final String preprocess_Pipeline = "/home/heli/main/gt/script/preprocess"; // not sure if this is correct, anyway it seems the var is not in use
+  	public static final String preprocess_Loc = "/opt/smi/sme/bin/preprocess";
+  	public static final String lookup_2cgLoc = " | /opt/smi/sme/bin/lookup2cg | ";
 
-    public String inputfile_Loc = "/home/teaksta/output/cg3input";
-    public String outputfile_Loc = "/home/teaksta/output/cg3output";
+    public static String inputfile_Loc = "/home/teaksta/output/cg3input";
+    public static String outputfile_Loc = "/home/teaksta/output/cg3output";
 }
