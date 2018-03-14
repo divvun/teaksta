520b1a4387f87e84dbbcb7409c2534b656d0f63b
chiara <chiara@gtsvn.uit.no>
2018-03-14 14:05:02 +0000

    if fst generates multiple forms, they are all accepted as correct input in cloze exercise for SubstantiveSingular. All forms are also displayed by clicking the hint button
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@164643 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/resources/firefox-extension/werti/chrome/content/substantivesingular.js
+++ sme/src/main/resources/firefox-extension/werti/chrome/content/substantivesingular.js
@@ -6,20 +6,20 @@
 	MAX_MC: 5,
 	// actual number of items in combobox in mc (value is overridden below)
 	maxLength: 5,
-	
+
 	// candidates for mc options presented to user
 	types: [],
 	hitList: [],
-	
+
 	// the total number of clicks
 	totalclicked : 0,
-	
+
 	// the total number of correct clicks
 	totalresult : 0,
-		
+
 	remove: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 		$('body').undelegate('span.wertiviewtoken', 'click', wertiview.substantivesingular.clickHandler);
@@ -27,7 +27,7 @@
 		$('body').undelegate('span.wertiviewhint', 'click', wertiview.substantivesingular.clozeHintHandler);
 		$('body').undelegate('input.wertiviewinput', 'change', wertiview.substantivesingular.clozeInputHandler);
 		$('body').undelegate('input.wertiviewhint', 'click', wertiview.substantivesingular.clozeHintHandler);  // was: span.wertiviewhint
-		
+
 		$('.wertiviewinput').each( function() {
 			$(this).replaceWith($(this).data('wertiviewanswer'));
 		});
@@ -37,19 +37,19 @@
 
 	colorize: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 		$('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiveSingular');
 	},
-	
+
 	colorizeSpan: function(span, topic) {
 span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiveSingular');
 	},
 
 	click: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 		// change all wertiviewtoken spans to mouseover pointer
@@ -69,27 +69,27 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 
 		// correct cursor inside wertiviewtokens within multi-word spans
 		//$('span.wertiviewRELEVANT').find('span.wertiviewconjunction').css({'cursor': 'text'});
-		
+
 		// handle click
-		$('body').delegate('span.wertiviewtoken', 'click', {context: contextDoc}, wertiview.substantivesingular.clickHandler); 
+		$('body').delegate('span.wertiviewtoken', 'click', {context: contextDoc}, wertiview.substantivesingular.clickHandler);
 	},
 
 	clickHandler: function(event) {
 		var contextDoc = event.data.context;
 
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
-		
+
 		if($(this).hasClass('wertiviewSubstantiveSingular')) {  // was: wertiviewhit
 			$(this).addClass('clickStyleCorrect');
             wertiview.substantivesingular.updateClickLog(event, "click", 1, $(this).text(), "");
 		} else {
 			$(this).addClass('clickStyleIncorrect');
 			wertiview.substantivesingular.updateClickLog(event, "click", 0, $(this).text(), "");
-		} 
+		}
 		//$(this).css({'cursor': 'auto'});
-        
+
 		// not within a relevant phrase
 		/*if ($(this).parents('.wertiviewRELEVANT').length == 0) {
 			$(this).addClass('clickStyleIncorrect');
@@ -110,13 +110,13 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 		// TODO: if this is a clue
 		var isClue = false;
 
-		if ($(this).hasClass('wertiviewCLU-BOTHMEANDIFF') || $(this).hasClass('wertiviewCLU-BOTHMEANSAME') || 
-				$(this).hasClass('wertiviewCLU-FIXEDEXP') || $(this).hasClass('wertiviewCLU-GERONLY') || 
-				$(this).hasClass('wertiviewCLU-INFONLY') || 
+		if ($(this).hasClass('wertiviewCLU-BOTHMEANDIFF') || $(this).hasClass('wertiviewCLU-BOTHMEANSAME') ||
+				$(this).hasClass('wertiviewCLU-FIXEDEXP') || $(this).hasClass('wertiviewCLU-GERONLY') ||
+				$(this).hasClass('wertiviewCLU-INFONLY') ||
 				$(this).find('.wertiviewCLU-BOTHMEANDIFF, .wertiviewCLU-BOTHMEANSAME, .wertiviewCLU-FIXEDEXP, .wertiviewCLU-GERONLY, .wertiviewCLU-INFONLY').length > 0) {
 			isClue = true;
 		}
-		
+
 		if (isClue) {
 			$(this).addClass('clickStyleCorrect');
 		} else {
@@ -124,15 +124,15 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 		}*/
 		return false;
 	},
-	
+
 	mc: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
-		
+
 		// get potential spans
 		var $hits = $('span.wertiviewSubstantiveSingular');
-		
+
 		//var hitList = [];
 		var tokens = [];
 		wertiview.substantivesingular.types = [];
@@ -156,7 +156,7 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 			wertiview.substantivesingular.maxLength = wertiview.substantivesingular.types.length;
 		}*/
 
-		/* 
+		/*
 		$hits.each( function() {
 			// if this is a split infinitive, skip
 			if ($(this).find('.wertiviewINFSPLIT').length == 0) {
@@ -167,43 +167,43 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 						return;
 					}
 				}
-				hitList.push($(this));				
-			} 
+				hitList.push($(this));
+			}
 		}); */
 
-		wertiview.activity.mc(contextDoc, wertiview.substantivesingular.hitList, 
-				wertiview.substantivesingular.clozeInputHandler, 
-				wertiview.substantivesingular.clozeHintHandler, 
-				wertiview.substantivesingular.mcGetOptions, 
+		wertiview.activity.mc(contextDoc, wertiview.substantivesingular.hitList,
+				wertiview.substantivesingular.clozeInputHandler,
+				wertiview.substantivesingular.clozeHintHandler,
+				wertiview.substantivesingular.mcGetOptions,
 				wertiview.substantivesingular.mcGetCorrectAnswer);
 
 	},
-	
+
 	mcGetOptions: function($hit, capType){
 		var options = [];
 		var j = 0;
 		// Get the list of distractors for the given hit (they are saved as a space-separated list in the attribute "distractors" of the wertiview span tag):
 		wertiview.substantivesingular.types = $hit.attr('distractors').split(" ");
 	    wertiview.lib.shuffleList(wertiview.substantivesingular.types);
-        
+
         // Add the distractor forms to the options list:
         while (j < wertiview.substantivesingular.types.length && options.length < wertiview.substantivesingular.MAX_MC - 1) {
             // The forms that are homonymous to the correct form are excluded from the list of options:
-            if (wertiview.substantivesingular.types[j] != $hit.text().toLowerCase() && wertiview.substantivesingular.types[j] != "") 
+            if (wertiview.substantivesingular.types[j] != $hit.text().toLowerCase() && wertiview.substantivesingular.types[j] != "")
             {
                 var homonym = false;
                 var k = 0;
                 while (k < j) //check for homonymes among the distractors
-                { 
+                {
                     if (wertiview.substantivesingular.types[k] == wertiview.substantivesingular.types[j])
                         homonym = true;
                     k++;
-                } 
-                if (!homonym)                 options.push(wertiview.lib.matchCapitalization(wertiview.substantivesingular.types[j], capType)); 
+                }
+                if (!homonym)                 options.push(wertiview.lib.matchCapitalization(wertiview.substantivesingular.types[j], capType));
             }
 			j++;
 		}
-		
+
 		options.push(wertiview.lib.matchCapitalization($hit.text(), capType));
 		wertiview.lib.shuffleList(options);
 		return options;
@@ -211,38 +211,42 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 		//var options = $hit.attr('title').split(";");
 		//return options;
 	},
-	
+
 	mcGetCorrectAnswer: function($hit, capType){
 		return $hit.text();
 	},
-	
+
+	clozeGetCorrectAnswer: function($hit, capType){
+		return $hit.attr('possibleforms');
+	},
+
 	cloze: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
-		
+
 		// get potential spans
 		//var $hits = $('span.wertiviewRELEVANT').find('span.wertiviewconjunction');
 		var $hits = $('span.wertiviewSubstantiveSingular');
 
 		var hitList = [];
 		$hits.each( function() {
-			hitList.push($(this));				
-		}); 
+			hitList.push($(this));
+		});
 		/*$hits.each( function() {
 			// if this is a split infinitive, skip
 			if ($(this).find('.wertiviewINFSPLIT').length == 0) {
-				hitList.push($(this));				
+				hitList.push($(this));
 			}
 		});*/
 
-		wertiview.activity.cloze(contextDoc, hitList, 
-				wertiview.substantivesingular.clozeInputHandler, 
-				wertiview.substantivesingular.clozeHintHandler, 
-				wertiview.substantivesingular.mcGetCorrectAnswer,
+		wertiview.activity.cloze(contextDoc, hitList,
+				wertiview.substantivesingular.clozeInputHandler,
+				wertiview.substantivesingular.clozeHintHandler,
+				wertiview.substantivesingular.clozeGetCorrectAnswer,
 				wertiview.substantivesingular.clozeAddBaseform);
 	},
-	
+
 	clozeAddBaseform: function($hit, capType, $){
 		// create baseform info
 		var $baseform = $('<span>');
@@ -257,46 +261,79 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 	clozeInputHandler: function(event) {
 		var jQuery = wertiview.jQuery;
 		var contextDoc = event.data.context;
-		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		  $.fn = $.prototype = jQuery.fn;
 
 		var nextInput;
 
-		// if the answer is correct, turn into text, else color text within input
-		if($(this).val().toLowerCase() == $(this).data('wertiviewanswer').toLowerCase()) {
-			$text = $("<span>");
-			$text.addClass('wertiview');
-			$text.addClass('clozeStyleCorrect');
-			$text.text($(this).data('wertiviewanswer'));
-			if($(this).data('wertiviewnexthit')) {   
-				nextInput = $(this).data('wertiviewnexthit');
+		//check if the user input match one of the possible answers
+		var answer_split = $(this).data('wertiviewanswer').toLowerCase().split(" ");
+		var correct_answer = false;
+		for (i = 0; i < answer_split.length; i++) {
+			if ($(this).val().toLowerCase() == answer_split[i]) {
+				correct_answer = true;
 			}
-			wertiview.lib.replaceInput($(this).parent(), $text);
-			wertiview.substantivesingular.updateClickLog(event, "cloze", 1, $(this).val(), $(this).val());
+		}
+
+		// if the answer is correct, turn into text, else color text within input
+		//
+		//if($(this).val().toLowerCase() == $(this).data('wertiviewanswer').toLowerCase()) {
+		//	$text = $("<span>");
+		//	$text.addClass('wertiview');
+		//	$text.addClass('clozeStyleCorrect');
+		//	$text.text($(this).data('wertiviewanswer'));
+		//	if($(this).data('wertiviewnexthit')) {
+		//		nextInput = $(this).data('wertiviewnexthit');
+		//	}
+		//	wertiview.lib.replaceInput($(this).parent(), $text);
+			//The following line was commented on the gtoahpa version but not on svn version
+			//wertiview.substantivesingular.updateClickLog(event, "cloze", 1, $(this).val(), $(this).val());
 			/*// focus next input
 			if(nextInput) {
 				$("#" + nextInput).get(0).focus();
 			}*/
+		//} else {
+		//	$(this).addClass('clozeStyleIncorrect');
+			//The following line was commented on the gtoahpa version but not on svn version
+			//wertiview.substantivesingular.updateClickLog(event, "cloze", 0, $(this).val(), $(this).data('wertiviewanswer'));
+		//}
+		// if the answer is correct, turn into text, else color text within input
+		if(correct_answer) {
+			$text = $("<span>");
+			$text.addClass('wertiview');
+			$text.addClass('clozeStyleCorrect');
+			//#$text.text($(this).data('wertiviewanswer'));
+			$text.text($(this).val().toLowerCase());
+			if($(this).data('wertiviewnexthit')) {
+				nextInput = $(this).data('wertiviewnexthit');
+			}
+			wertiview.lib.replaceInput($(this).parent(), $text);
+			//The following line was commented on the gtoahpa version but not on svn version
+			//wertiview.substantivesingular.updateClickLog(event, "cloze", 1, $(this).val(), $(this).val());
 		} else {
-			$(this).addClass('clozeStyleIncorrect');
-			wertiview.substantivesingular.updateClickLog(event, "cloze", 0, $(this).val(), $(this).data('wertiviewanswer'));
-		}
+				$(this).addClass('clozeStyleIncorrect');
+				//The following line was commented on the gtoahpa version but not on svn version
+				//wertiview.substantivesingular.updateClickLog(event, "cloze", 0, $(this).val(), $(this).data('wertiviewanswer'));
+			}
 	},
 
 	clozeHintHandler: function(event) {
 		var jQuery = wertiview.jQuery;
 		var contextDoc = event.data.context;
-		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		  $.fn = $.prototype = jQuery.fn;
 
 		var nextInput;
 
 		// fill in the answer by replacing input with text
+		//$text = $("<span>");
+		var answer_sep = $(this).prev().data('wertiviewanswer').replace(/\s/g, "/");
 		$text = $("<span>");
 		$text.addClass('wertiview');
 		$text.addClass('clozeStyleProvided');
-		$text.text($(this).prev().data('wertiviewanswer'));
-		if($(this).prev().data('wertiviewnexthit')) {  
+		//$text.text($(this).prev().data('wertiviewanswer'));
+		$text.text(answer_sep);
+		if($(this).prev().data('wertiviewnexthit')) {
 			nextInput = $(this).prev().data('wertiviewnexthit');
 		}
 		wertiview.lib.replaceInput($(this).parent(), $text);
@@ -305,21 +342,21 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 		if(nextInput) {
 			$("#" + nextInput).get(0).focus();
 		}*/
-		
+
 		return false;
 	},
-	
+
 	updateClickLog: function(event, extype, result, word, facit) {
 	   var jQuery = wertiview.jQuery;
 	   var contextDoc = event.data.context;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 	   var nr_of_hits = $('span.wertiviewSubstantiveSingular').length;
 	   wertiview.substantivesingular.totalresult += result;
 	   wertiview.substantivesingular.totalclicked++;
 	   var correctclicks = wertiview.substantivesingular.totalresult;
-	   var totalclicks = wertiview.substantivesingular.totalclicked; 
+	   var totalclicks = wertiview.substantivesingular.totalclicked;
 	   var ratio1 =  correctclicks / totalclicks;
 	   var ratio2 = correctclicks / nr_of_hits;
 	   var dataString = "correct clicks / total clicks " + ratio1 + ", correct clicks / hits on the page " + ratio2;
@@ -339,4 +376,3 @@ span.find('span.wertiviewSubstantiveSingular').addClass('colorizeStyleSubstantiv
 	   //alert("correct clicks / hits on the page" + ratio2);
     }
  	};
-
