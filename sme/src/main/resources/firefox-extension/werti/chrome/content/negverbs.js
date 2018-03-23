4a7e95d099820b92aea3c528c9475b6ef07e9d2b
chiara <chiara@gtsvn.uit.no>
2018-03-23 14:03:41 +0000

    accept all possible forms in cloze exercise for ConNeg
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@165010 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/resources/firefox-extension/werti/chrome/content/negverbs.js
+++ sme/src/main/resources/firefox-extension/werti/chrome/content/negverbs.js
@@ -6,14 +6,14 @@
 	MAX_MC: 5,
 	// actual number of items in combobox in mc (value is overridden below)
 	maxLength: 5,
-	
+
 	// candidates for mc options presented to user
 	types: [],
 	hitList: [],
-		
+
 	remove: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 		$('body').undelegate('span.wertiviewtoken', 'click', wertiview.negverbs.clickHandler);
@@ -21,7 +21,7 @@
 		$('body').undelegate('span.wertiviewhint', 'click', wertiview.negverbs.clozeHintHandler);
 		$('body').undelegate('input.wertiviewinput', 'change', wertiview.negverbs.clozeInputHandler);
 		$('body').undelegate('input.wertiviewhint', 'click', wertiview.negverbs.clozeHintHandler);  // was: span.wertiviewhint
-		
+
 		$('.wertiviewinput').each( function() {
 			$(this).replaceWith($(this).data('wertiviewanswer'));
 		});
@@ -31,57 +31,57 @@
 
 	colorize: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 		$('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 	},
-	
+
 	colorizeSpan: function(span, topic) {
 span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 	},
 
 	click: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
 
 		// change all wertiviewtoken spans to mouseover pointer
-		$('span.wertiviewtoken').css({'cursor': 'pointer'}); 
+		$('span.wertiviewtoken').css({'cursor': 'pointer'});
 
 		// conjunction markup
 		$('span.wertiviewRELEVANT').find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 
 		// correct cursor inside wertiviewtokens within multi-word spans
 		//$('span.wertiviewRELEVANT').find('span.wertiviewconjunction').css({'cursor': 'text'});
-		
+
 		// handle click
-		$('body').delegate('span.wertiviewtoken', 'click', {context: contextDoc}, wertiview.negverbs.clickHandler); 
+		$('body').delegate('span.wertiviewtoken', 'click', {context: contextDoc}, wertiview.negverbs.clickHandler);
 	},
 
 	clickHandler: function(event) {
 		var contextDoc = event.data.context;
 
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
-		
+
 		if($(this).hasClass('wertiviewConNeg')) {  // was: wertiviewhit
 			$(this).addClass('clickStyleCorrect');
 		} else {
 			$(this).addClass('clickStyleIncorrect');
-		} 
+		}
         return false;
 	},
-	
+
 	mc: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
-		
+
 		// get potential spans
 		var $hits = $('span.wertiviewConNeg');
-		
+
 		//var hitList = [];
 		var tokens = [];
 		wertiview.negverbs.types = [];  // answer options that will be displayed  as a drop-down list
@@ -105,7 +105,7 @@ span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 			wertiview.negverbs.maxLength = wertiview.negverbs.types.length;
 		}*/
 
-		/* 
+		/*
 		$hits.each( function() {
 			// if this is a split infinitive, skip
 			if ($(this).find('.wertiviewINFSPLIT').length == 0) {
@@ -116,32 +116,32 @@ span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 						return;
 					}
 				}
-				hitList.push($(this));				
-			} 
+				hitList.push($(this));
+			}
 		}); */
 
-		wertiview.activity.mc(contextDoc, wertiview.negverbs.hitList, 
-				wertiview.negverbs.clozeInputHandler, 
-				wertiview.negverbs.clozeHintHandler, 
-				wertiview.negverbs.mcGetOptions, 
+		wertiview.activity.mc(contextDoc, wertiview.negverbs.hitList,
+				wertiview.negverbs.clozeInputHandler,
+				wertiview.negverbs.clozeHintHandler,
+				wertiview.negverbs.mcGetOptions,
 				wertiview.negverbs.mcGetCorrectAnswer);
 
 	},
-	
+
 	mcGetOptions: function($hit, capType){
 		var options = [];
 		var j = 0;
 		// Get the list of distractors for the given hit (they are saved as a space-separated list in the attribute "distractors" of the wertiview span tag):
 		wertiview.negverbs.types = $hit.attr('distractors').split(" ");
         wertiview.lib.shuffleList(wertiview.negverbs.types);
-        
+
         // Add the distractor forms to the options list:
         while (options.length < wertiview.negverbs.maxLength - 1) {
             // The forms that are homonymous to the correct form are excluded from the list of options:
             if (wertiview.negverbs.types[j] != $hit.text().toLowerCase() && wertiview.negverbs.types[j] != "") {
-            options.push(wertiview.lib.matchCapitalization(wertiview.negverbs.types[j], capType)); 
+            options.push(wertiview.lib.matchCapitalization(wertiview.negverbs.types[j], capType));
             }
-		
+
 		/*while (options.length < wertiview.negverbs.maxLength - 1) {
 			if (wertiview.negverbs.types[j] != $hit.text().toLowerCase()) {
 				options.push(wertiview.lib.matchCapitalization(wertiview.negverbs.types[j], capType));
@@ -149,47 +149,51 @@ span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
         */
 			j++;
 		}
-		
+
 		options.push(wertiview.lib.matchCapitalization($hit.text(), capType));
-		
+
 		wertiview.lib.shuffleList(options);
 		return options;
 
 		//var options = $hit.attr('title').split(";");
 		//return options;
 	},
-	
+
 	mcGetCorrectAnswer: function($hit, capType){
 		return $hit.text();
 	},
-	
+
+	clozeGetCorrectAnswer: function($hit, capType){
+		return $hit.attr('possibleforms');
+	},
+
 	cloze: function(contextDoc) {
 		var jQuery = wertiview.jQuery;
-		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		$.fn = $.prototype = jQuery.fn;
-		
+
 		// get potential spans
 		//var $hits = $('span.wertiviewRELEVANT').find('span.wertiviewconjunction');
 		var $hits = $('span.wertiviewConNeg');
 
 		var hitList = [];
 		$hits.each( function() {
-			hitList.push($(this));				
-		}); 
+			hitList.push($(this));
+		});
 		/*$hits.each( function() {
 			// if this is a split infinitive, skip
 			if ($(this).find('.wertiviewINFSPLIT').length == 0) {
-				hitList.push($(this));				
+				hitList.push($(this));
 			}
 		});*/
 
-		wertiview.activity.cloze(contextDoc, hitList, 
-				wertiview.negverbs.clozeInputHandler, 
-				wertiview.negverbs.clozeHintHandler, 
-				wertiview.negverbs.mcGetCorrectAnswer,
+		wertiview.activity.cloze(contextDoc, hitList,
+				wertiview.negverbs.clozeInputHandler,
+				wertiview.negverbs.clozeHintHandler,
+				wertiview.negverbs.clozeGetCorrectAnswer,
 				wertiview.negverbs.clozeAddBaseform);
 	},
-	
+
 	clozeAddBaseform: function($hit, capType, $){
 		// create baseform info
 		var $baseform = $('<span>');
@@ -204,18 +208,26 @@ span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 	clozeInputHandler: function(event) {
 		var jQuery = wertiview.jQuery;
 		var contextDoc = event.data.context;
-		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
+		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 		  $.fn = $.prototype = jQuery.fn;
 
 		var nextInput;
 
+		//check if the user input match one of the possible answers
+		var answer_split = $(this).data('wertiviewanswer').toLowerCase().split(" ");
+		var correct_answer = false;
+		for (i = 0; i < answer_split.length; i++) {
+			if ($(this).val().toLowerCase() == answer_split[i]) {
+				correct_answer = true;
+			}
+		}
 		// if the answer is correct, turn into text, else color text within input
-		if($(this).val().toLowerCase() == $(this).data('wertiviewanswer').toLowerCase()) {
+		if(correct_answer) {
 			$text = $("<span>");
 			$text.addClass('wertiview');
 			$text.addClass('clozeStyleCorrect');
-			$text.text($(this).data('wertiviewanswer'));
-			if($(this).data('wertiviewnexthit')) {   
+			$text.text($(this).val().toLowerCase());
+			if($(this).data('wertiviewnexthit')) {
 				nextInput = $(this).data('wertiviewnexthit');
 			}
 			wertiview.lib.replaceInput($(this).parent(), $text);
@@ -225,24 +237,29 @@ span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 				$("#" + nextInput).get(0).focus();
 			}*/
 		} else {
-			$(this).addClass('clozeStyleIncorrect');
-		}
+				$(this).addClass('clozeStyleIncorrect');
+			}
 	},
 
 	clozeHintHandler: function(event) {
 		var jQuery = wertiview.jQuery;
 		var contextDoc = event.data.context;
-		  var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.content.document); };
-		  $.fn = $.prototype = jQuery.fn;
+		var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
+		$.fn = $.prototype = jQuery.fn;
 
 		var nextInput;
 
 		// fill in the answer by replacing input with text
+		//$text = $("<span>");
+
+		//fill in the answer by replacing input with text where each answer is separated by /
+		var answer_sep = $(this).prev().data('wertiviewanswer').replace(/\s/g, "/");
 		$text = $("<span>");
 		$text.addClass('wertiview');
 		$text.addClass('clozeStyleProvided');
-		$text.text($(this).prev().data('wertiviewanswer'));
-		if($(this).prev().data('wertiviewnexthit')) {  
+		//$text.text($(this).prev().data('wertiviewanswer'));
+		$text.text(answer_sep);
+		if($(this).prev().data('wertiviewnexthit')) {
 			nextInput = $(this).prev().data('wertiviewnexthit');
 		}
 		wertiview.lib.replaceInput($(this).parent(), $text);
@@ -251,8 +268,7 @@ span.find('span.wertiviewConNeg').addClass('colorizeStyleNegVerbs');
 		if(nextInput) {
 			$("#" + nextInput).get(0).focus();
 		}*/
-		
+
 		return false;
 	}
 	};
-
