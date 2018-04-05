fc4785ec7fa4a22e0d58290cebbc7b69a8f7a2ad
chiara <chiara@gtsvn.uit.no>
2018-04-05 12:13:59 +0000

    Check if werti spans are in menu links or in elements with widget class, so that these are not used for the exercise. Also, removed some old code commented out
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@165237 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/resources/firefox-extension/werti/chrome/content/infiniteverbs.js
+++ sme/src/main/resources/firefox-extension/werti/chrome/content/infiniteverbs.js
@@ -25,7 +25,6 @@
 			$('.wertiviewinput').each( function() {
 				$(this).replaceWith($(this).data('wertiviewanswer'));
 			});
-			//$('span.wertiviewbaseform').remove();
 			$('.wertiviewhint').remove();
 		},
 
@@ -34,7 +33,26 @@
 			var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 			$.fn = $.prototype = jQuery.fn;
 
-			$('span.wertiviewInfiniteVerb').addClass('colorizeStyleInfiniteVerbs');
+			// check if the span is in a menu link or in a widget, change remaining wertiviewtoken spans to mouseover pointer
+			var spanTags = document.getElementsByClassName('wertiview');
+			for (i = 0; i < spanTags.length; i++) {
+				if (spanTags[i].parentElement.classList) {
+					var patt_widget = /widget/g;
+	    		var res_widget = patt_widget.test(spanTags[i].parentElement.classList);
+					if (spanTags[i].parentElement.tagName != 'A' && !res_widget)  {
+						if (spanTags[i].hasChildNodes()) {
+				  		var children = spanTags[i].childNodes;
+							for (var k = 0; k < children.length; k++) {
+								if (children[k].classList) {
+									if (children[k].classList.contains('wertiviewInfiniteVerb')) {
+										children[k].classList.add('colorizeStyleInfiniteVerbs');
+									}
+								}
+		  				}
+						}
+					}
+				}
+			}
 		},
 
 		colorizeSpan: function(span, topic) {
@@ -46,14 +64,24 @@
 			var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 			$.fn = $.prototype = jQuery.fn;
 
-			// change all wertiviewtoken spans to mouseover pointer
-			$('span.wertiviewtoken').css({'cursor': 'pointer'});
-
-			// conjunction markup
-			$('span.wertiviewRELEVANT').find('span.wertiviewInfiniteVerb').addClass('colorizeStyleInfiniteVerbs');
-
-			// correct cursor inside wertiviewtokens within multi-word spans
-			//$('span.wertiviewRELEVANT').find('span.wertiviewconjunction').css({'cursor': 'text'});
+			// check if the span is in a menu link or in a widget, change remaining wertiviewtoken spans to mouseover pointer
+			var spanTags = document.getElementsByClassName('wertiview');
+			for (i = 0; i < spanTags.length; i++) {
+				if (spanTags[i].parentElement.classList) {
+					var patt_widget = /widget/g;
+					var res_widget = patt_widget.test(spanTags[i].parentElement.classList);
+					if (spanTags[i].parentElement.tagName != 'A' && !res_widget)  {
+						if (spanTags[i].hasChildNodes()) {
+							var children = spanTags[i].childNodes;
+							for (var k = 0; k < children.length; k++) {
+								if (children[k].classList) {
+									children[k].style.cursor = "pointer"; //({'cursor': 'pointer'});
+								}
+							}
+						}
+					}
+				}
+			}
 
 			// handle click
 			$('body').delegate('span.wertiviewtoken', 'click', {context: contextDoc}, wertiview.infiniteverbs.clickHandler);
@@ -66,45 +94,43 @@
 			var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 			$.fn = $.prototype = jQuery.fn;
 
-			if($(this).hasClass('wertiviewInfiniteVerb')) {  // was: wertiviewhit
-				$(this).addClass('clickStyleCorrect');
-			} else {
-					$(this).addClass('clickStyleIncorrect');
+			//check if parent element is a link, a menu item or a widget
+			var parent_elements = [];
+			var current_element = this;
+			while (current_element) {
+		    parent_elements.unshift(current_element);
+		    current_element = current_element.parentNode;
+			}
+			var widget_found = false;
+			var menu_found = false;
+			var a_found = false;
+
+			for (i = 0; i < parent_elements.length; i++) {
+				var patt_widget = /widget/g;
+				var res_widget = patt_widget.test(parent_elements[i].classList);
+				var patt_menu = /menu/g;
+				var res_menu = patt_menu.test(parent_elements[i].classList);
+				if (res_widget) {
+					widget_found = true;
 				}
-			//$(this).css({'cursor': 'auto'});
-
-			// not within a relevant phrase
-			/*if ($(this).parents('.wertiviewRELEVANT').length == 0) {
-				$(this).addClass('clickStyleIncorrect');
-				return false;
+				if (res_menu) {
+					menu_found = true;
+				}
+				if (parent_elements[i].tagName == 'A') {
+					a_found = true;
+				}
 			}
 
-			// an already colored conjunction
-			var isColored = false;
-
-			if ($(this).hasClass('wertiviewconjunction') || $(this).find('.wertiviewconjunction').length > 0) {
-				isColored = true;
+			if (!widget_found && !menu_found && !a_found) {
+				if (this.classList.contains('wertiviewInfiniteVerb')) {  // was: wertiviewhit
+					this.classList.add('clickStyleCorrect');
+					wertiview.infiniteverbs.updateClickLog(event, "click", 1, $(this).text(), "");
+				} else {
+					this.classList.add('clickStyleIncorrect');
+					wertiview.infiniteverbs.updateClickLog(event, "click", 0, $(this).text(), "");
+				}
 			}
 
-			if (isColored) {
-				return false;
-			}
-
-			// TODO: if this is a clue
-			var isClue = false;
-
-			if ($(this).hasClass('wertiviewCLU-BOTHMEANDIFF') || $(this).hasClass('wertiviewCLU-BOTHMEANSAME') ||
-					$(this).hasClass('wertiviewCLU-FIXEDEXP') || $(this).hasClass('wertiviewCLU-GERONLY') ||
-					$(this).hasClass('wertiviewCLU-INFONLY') ||
-					$(this).find('.wertiviewCLU-BOTHMEANDIFF, .wertiviewCLU-BOTHMEANSAME, .wertiviewCLU-FIXEDEXP, .wertiviewCLU-GERONLY, .wertiviewCLU-INFONLY').length > 0) {
-				isClue = true;
-			}
-
-			if (isClue) {
-				$(this).addClass('clickStyleCorrect');
-			} else {
-				$(this).addClass('clickStyleIncorrect');
-			}*/
 			return false;
 		},
 
@@ -113,46 +139,44 @@
 			var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 			$.fn = $.prototype = jQuery.fn;
 
-			// get potential spans
-			var $hits = $('span.wertiviewInfiniteVerb');
-
-			//var hitList = [];
 			var tokens = [];
 			wertiview.infiniteverbs.types = [];
 			wertiview.infiniteverbs.hitList = [];
-			//alert($hits.length+" hits");
-			$hits.each( function() {
-				wertiview.infiniteverbs.hitList.push($(this));
-				//alert($(this).text());
-				tokens[$(this).text().toLowerCase()] = 1;
-			});
-			//alert("number of tokens: "+tokens.length);
-			//alert("size of hitList: "+wertiview.infiniteverbs.hitList.length);
-			/*for (word in tokens) {
-				wertiview.infiniteverbs.types.push(word);
-				//alert("word: "+word);
-			}*/
-			//alert(wertiview.infiniteverbs.types.length+" different infiniteverbs on page");
+
+			var spanTags = $('span.wertiviewInfiniteVerb');
+			//check if parent element is a link, a menu item or a widget
+			spanTags.each(function(){
+				var parent_elements = [];
+				var current_element = $(this)[0];
+				while (current_element) {
+					parent_elements.unshift(current_element);
+					current_element = current_element.parentNode;
+				}
+				var widget_found = false;
+				var menu_found = false;
+				var a_found = false;
+
+				for (i = 0; i < parent_elements.length; i++) {
+					var patt_widget = /widget/g;
+					var res_widget = patt_widget.test(parent_elements[i].classList);
+					var patt_menu = /menu/g;
+					var res_menu = patt_menu.test(parent_elements[i].classList);
+					if (res_widget) {
+						widget_found = true;
+					}
+					if (res_menu) {
+						menu_found = true;
+					}
+					if (parent_elements[i].tagName == 'A') {
+						a_found = true;
+					}
+				}
+				if (!widget_found && !menu_found && !a_found) {
+						wertiview.infiniteverbs.hitList.push($(this));
+						tokens[$(this).text().toLowerCase()] = 1;
+				}});
 
 			wertiview.infiniteverbs.maxLength = wertiview.infiniteverbs.MAX_MC;
-			/*if (wertiview.infiniteverbs.maxLength > wertiview.infiniteverbs.types.length) {
-				wertiview.infiniteverbs.maxLength = wertiview.infiniteverbs.types.length;
-			}*/
-
-			/*
-			$hits.each( function() {
-				// if this is a split infinitive, skip
-				if ($(this).find('.wertiviewINFSPLIT').length == 0) {
-					var options = $(this).attr('title').split(";");
-					// if the infinitive or gerund isn't given in the markup, skip
-					for (var j = 0; j < options.length; j++) {
-						if (options[j] == 'null') {
-							return;
-						}
-					}
-					hitList.push($(this));
-				}
-			}); */
 
 			wertiview.activity.mc(contextDoc, wertiview.infiniteverbs.hitList,
 					wertiview.infiniteverbs.clozeInputHandler,
@@ -188,9 +212,6 @@
 			options.push(wertiview.lib.matchCapitalization($hit.text(), capType));
 			wertiview.lib.shuffleList(options);
 			return options;
-
-			//var options = $hit.attr('title').split(";");
-			//return options;
 		},
 
 		mcGetCorrectAnswer: function($hit, capType){
@@ -206,21 +227,39 @@
 			var $ = function(selector,context){ return new jQuery.fn.init(selector,contextDoc||window.document); };
 			$.fn = $.prototype = jQuery.fn;
 
-			// get potential spans
-			//var $hits = $('span.wertiviewRELEVANT').find('span.wertiviewconjunction');
-			var $hits = $('span.wertiviewInfiniteVerb');
-
 			var hitList = [];
-			$hits.each( function() {
-				hitList.push($(this));
-			});
+			var spanTags = $('span.wertiviewInfiniteVerb');
+			//check if parent element is a link, a menu item or a widget
+			spanTags.each(function(){
+			  var parent_elements = [];
+			  var current_element = $(this)[0];
+			  while (current_element) {
+			      parent_elements.unshift(current_element);
+			      current_element = current_element.parentNode;
+			  }
+			  var widget_found = false;
+			  var menu_found = false;
+			  var a_found = false;
 
-			/*$hits.each( function() {
-				// if this is a split infinitive, skip
-				if ($(this).find('.wertiviewINFSPLIT').length == 0) {
-					hitList.push($(this));
-				}
-			});*/
+			  for (i = 0; i < parent_elements.length; i++) {
+			    var patt_widget = /widget/g;
+			    var res_widget = patt_widget.test(parent_elements[i].classList);
+			    var patt_menu = /menu/g;
+			    var res_menu = patt_menu.test(parent_elements[i].classList);
+			    if (res_widget) {
+			      widget_found = true;
+			    }
+			    if (res_menu) {
+			      menu_found = true;
+			    }
+			    if (parent_elements[i].tagName == 'A') {
+			      a_found = true;
+			    }
+			  }
+			  if (!widget_found && !menu_found && !a_found) {
+			      hitList.push($(this));
+			  }
+			});
 
 			wertiview.activity.cloze(contextDoc, hitList,
 					wertiview.infiniteverbs.clozeInputHandler,
@@ -266,11 +305,6 @@
 					nextInput = $(this).data('wertiviewnexthit');
 				}
 				wertiview.lib.replaceInput($(this).parent(), $text);
-
-				/*// focus next input
-				if(nextInput) {
-					$("#" + nextInput).get(0).focus();
-				}*/
 			} else {
 					$(this).addClass('clozeStyleIncorrect');
 				}
@@ -284,9 +318,6 @@
 
 			var nextInput;
 
-			// fill in the answer by replacing input with text
-			//$text.text($(this).prev().data('wertiviewanswer'));
-
 			//fill in the answer by replacing input with text where each answer is separated by /
 			var answer_sep = $(this).prev().data('wertiviewanswer').replace(/\s/g, "/");
 			$text = $("<span>");
@@ -297,11 +328,6 @@
 				nextInput = $(this).prev().data('wertiviewnexthit');
 			}
 			wertiview.lib.replaceInput($(this).parent(), $text);
-
-			/*// focus next input
-			if(nextInput) {
-				$("#" + nextInput).get(0).focus();
-			}*/
 
 			return false;
 		}
