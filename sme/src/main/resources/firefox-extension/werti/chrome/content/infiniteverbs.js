20aa8f453e7e945c1b67e5f5de2374c074dc041f
chiara <chiara@gtsvn.uit.no>
2018-04-05 13:10:19 +0000

    updateCliclLog is not defined here but only in substantivesingular.js
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@165243 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/resources/firefox-extension/werti/chrome/content/infiniteverbs.js
+++ sme/src/main/resources/firefox-extension/werti/chrome/content/infiniteverbs.js
@@ -124,10 +124,8 @@
 			if (!widget_found && !menu_found && !a_found) {
 				if (this.classList.contains('wertiviewInfiniteVerb')) {  // was: wertiviewhit
 					this.classList.add('clickStyleCorrect');
-					wertiview.infiniteverbs.updateClickLog(event, "click", 1, $(this).text(), "");
 				} else {
 					this.classList.add('clickStyleIncorrect');
-					wertiview.infiniteverbs.updateClickLog(event, "click", 0, $(this).text(), "");
 				}
 			}
 
