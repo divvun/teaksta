ca0b6d00317d376ec636ec77c5e5582a73fbbeb5
chiara <chiara@gtsvn.uit.no>
2018-04-12 09:36:35 +0000

    remove restriction on Nom
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@165477 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/java/werti/uima/enhancer/Vislcg3SubjectEnhancer.java
+++ sme/src/main/java/werti/uima/enhancer/Vislcg3SubjectEnhancer.java
@@ -21,11 +21,13 @@ import werti.util.EnhancerUtils;
 import werti.util.StringListIterable;
 import werti.server.WERTiServlet;
 
+import werti.util.Constants;
+
 /**
  * Use the TAG-B TAG-I sequences resulting from the CG3 analysis with
- * {@link werti.ae.Vislcg3Annotator} to enhance spans corresponding 
+ * {@link werti.ae.Vislcg3Annotator} to enhance spans corresponding
  * to the tags specified by the activity as tags of negation forms of verbs.
- * 
+ *
  * @author Niels Ott?
  * @author Adriane Boyd
  * @author Heli Uibo
@@ -35,13 +37,13 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 
 	private static final Logger log =
 		Logger.getLogger(Vislcg3SubjectEnhancer.class);
-	
+
 	private List<String> subjectTags;
 	private static String CHUNK_BEGIN_SUFFIX = "-B";
 	private static String CHUNK_INSIDE_SUFFIX = "-I";
-        private final String lookupLoc = "/usr/local/bin/lookup";
-        private final String lookupFlags = "-flags mbTT -utf8";
-	
+	private final String lookupLoc = Constants.lookup_Loc;
+  private final String lookupFlags = Constants.lookup_Flags;
+
 	@Override
 	public void initialize(UimaContext context)
 			throws ResourceInitializationException {
@@ -54,7 +56,7 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 	public void process(JCas cas) throws AnalysisEngineProcessException {
 		log.info("Starting Subject enhancement");
 		String enhancement_type = WERTiServlet.enhancement_type; // colorize, click, mc or cloze - chosen by the user and sent to the servlet as a request parameter
-		
+
 		// stack for started enhancements (chunk)
 		// Stack<Enhancement> enhancements = new Stack<Enhancement>();
 		// keep track of ids for each annotation class
@@ -66,7 +68,7 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 
 		// iterating over chunkTags instead of classCounts.keySet() because it is important to control the order in which
 		// spans are enhanced
-		
+
 		for (String conT: subjectTags) {
 			FSIterator cgTokenIter = cas.getAnnotationIndex(CGToken.type).iterator();
 			// remember previous token so we can getEnd() from it (chunk)
@@ -81,13 +83,12 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 					continue;
 				    }
 				}
+				//log.info("next reading: "+reading);
 
 				// analyze reading(s)
 				for (int i=0; i < cgt.getReadings().size(); i++) { // Loop over all the readings. If there is one analysis that matches the tag pattern then the token will be selected for the exercise.
-				    CGReading reading = cgt.getReadings(i); 
-				
-				    //log.info("next reading: "+reading);
-				
+				    CGReading reading = cgt.getReadings(i);
+
 				    if (containsTag(reading, conT)) {
 					// get lemma from the CG reading
 					// String lemma = getLemma(reading); - not needed for exercises on syntactic functions
@@ -98,12 +99,12 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 					e.setRelevant(true);
 					e.setBegin(cgt.getBegin());
 					e.setEnd(cgt.getEnd());
-					
+
 					// increment id
 					newId = classCounts.get(conT) + 1;
 					String spanStartTag = "<span id=\"" + EnhancerUtils.get_id("WERTi-span-" + conT, newId) + "\" class=\"wertiviewtoken  wertiviewSubject \">";
 					//log.info(spanStartTag);
-					e.setEnhanceStart(spanStartTag);					
+					e.setEnhanceStart(spanStartTag);
 					e.setEnhanceEnd("</span>");
 					classCounts.put(conT, newId);
 					//log.info(newId);
@@ -119,21 +120,21 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 				//prev = cgt;
 			}
 		}
-		
+
 
 		// (chunk)
 		//log.info("Enhancement stack is "
 		//		+ (enhancements.empty() ? "empty, OK" : "not empty, WTF??"));
 		log.info("Finished subject enhancement");
 	}
-	
+
 	/*
 	 * Determines whether the given token is safe, i.e. unambiguous
 	 */
 	private boolean isSafe(CGToken t) {
 		return t.getReadings() != null && t.getReadings().size() == 1;
 	}
-	
+
 	/*
 	 * Determines whether the given reading contains the given tag
 	 */
@@ -143,8 +144,8 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 		for (String rtag : reading) {
 			reading_str = reading_str + rtag + " ";
 		}
-		
-		if (reading_str.contains(tag) && (reading_str.contains("Nom") || (tag.compareTo("SUBJ") != 0))) {  // Tag string contains the given tag sequence as a substring, plus it should be in the nominative case.
+
+		if (reading_str.contains(tag) ) {  // Tag string contains the given tag sequence as a substring, plus it should be in the nominative case.
             log.info(cgr + " contains " + tag);
             return true;
         }
@@ -152,7 +153,7 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 		//log.info(cgr + " does not contain " + tag);
 		return false;
 	}
-	
+
 	/*
 	private String getLemma(CGReading cgr) {
 		StringListIterable reading = new StringListIterable(cgr);
@@ -165,7 +166,7 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
             }
 		}
 		// Convert the lemma to utf8. - Not needed any more because the whole cg input and output is converted to utf8.
-		/* 
+		/*
 		try {
             byte[] b = lemma.getBytes();
             lemma_utf8 = new String(b,"UTF-8");
@@ -178,4 +179,3 @@ public class Vislcg3SubjectEnhancer extends JCasAnnotator_ImplBase {
 		/*return lemma;
 	} */
 }
-
