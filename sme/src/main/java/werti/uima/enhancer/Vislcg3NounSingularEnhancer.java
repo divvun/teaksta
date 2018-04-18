82f56981dff2bd0882c4a22cc8e8931a36800c97
chiara <chiara@gtsvn.uit.no>
2018-04-18 09:13:27 +0000

    remove part of analyses after @ which causing error in cloze
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@165714 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/java/werti/uima/enhancer/Vislcg3NounSingularEnhancer.java
+++ sme/src/main/java/werti/uima/enhancer/Vislcg3NounSingularEnhancer.java
@@ -444,7 +444,7 @@ public class Vislcg3NounSingularEnhancer extends JCasAnnotator_ImplBase {
 			String lemma_str = reading_str.substring(0, reading_str.indexOf("+"));
 			String an_tmp = reading_str.substring(reading_str.indexOf("+")+1, reading_str.length());
 			String analyses_str = an_tmp.replace("+<sme>", "");
-			//analyses_str = analyses_str.substring(0, analyses_str.indexOf("@")-1);
+			analyses_str = analyses_str.substring(0, analyses_str.indexOf("@")-1);
 			String lem_and_an = lemma_str + "+" + analyses_str + "\n";
 
 			return lem_and_an;
