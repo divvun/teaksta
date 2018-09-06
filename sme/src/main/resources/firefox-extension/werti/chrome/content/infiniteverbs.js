00ba6addd5f0ff3d65e67e1c932d08fae23adc5c
chiara <chiara@gtsvn.uit.no>
2018-09-06 08:32:48 +0000

    fix infiniteVerb mc and cloze exercises (js), so that only norm options/answer are shown/accepted
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/apps/teaksta@170427 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- sme/src/main/resources/firefox-extension/werti/chrome/content/infiniteverbs.js
+++ sme/src/main/resources/firefox-extension/werti/chrome/content/infiniteverbs.js
@@ -189,6 +189,7 @@
 			var j = 0;
 			// Get the list of distractors for the given hit (they are saved as a space-separated list in the attribute "distractors" of the wertiview span tag):
 			wertiview.infiniteverbs.types = $hit.attr('distractors').split(" ");
+			var correct_answer = $hit.attr('answer');
 	    wertiview.lib.shuffleList(wertiview.infiniteverbs.types);
 
 	    // Add the distractor forms to the options list:
@@ -207,13 +208,14 @@
 				j++;
 			}
 
-			options.push(wertiview.lib.matchCapitalization($hit.text(), capType));
+			options.push(wertiview.lib.matchCapitalization(correct_answer, capType));
 			wertiview.lib.shuffleList(options);
 			return options;
 		},
 
 		mcGetCorrectAnswer: function($hit, capType){
-			return $hit.text();
+			var correct_answer = $hit.attr('answer');
+			return correct_answer;
 		},
 
 		clozeGetCorrectAnswer: function($hit, capType){
